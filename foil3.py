77db5bc1804b78114e695fb3dda1a76321aec8eb
Rodrigo <rodrigo4zevedo@gmail.com>
2018-03-01 14:06:56 -0300

    Updated foil

--- foil3.py
+++ foil3.py
@@ -19,22 +19,43 @@ class Predicate:
     def __init__(self, name):
         self.name = name
            
-    def __str__(self):
-        return(self.name)
+    def __repr__(self):
+        return self.name
+    
+    def __hash__(self):
+        return hash(self.name)
+    
+    def __eq__(self, other):
+        return str(self) == str(other)
 
 class Atom:
     def __init__(self, name):
         self.name = name
         
-    def __str__(self):
+    def __repr__(self):
         return self.name
+    
+    def __hash__(self):
+        return hash(self.name)
+    
+    def __eq__(self, other):
+        return str(self) == str(other)
     
 class Variable:
     def __init__(self, name):
         self.name = name
         
-    def __str__(self):
+    def __repr__(self):
         return self.name
+    
+    def __int__(self):
+        return ord(self.name) - 65
+    
+    def __hash__(self):
+        return hash(self.name)
+    
+    def __eq__(self, other):
+        return str(self) == str(other)
     
 class Tuples:
     def __init__(self):
@@ -83,6 +104,89 @@ class Structure:
 
         self.tuples.add(rel, [sub_atom, obj_atom])
         
+    def count_tuples(self, target):
+        def recursive_count(root):
+            if len(root) == 0:
+                return 1
+            else:
+                s = 0
+                for tupl in root:
+                    s += recursive_count(root[tupl])
+                return s
+        tuples = self.tuples.data[target]
+        return recursive_count(tuples)
+    
+#    def satisty_clause_non_recursive(self, clause):
+#        for i in range(len(clause)):
+#            predicate = clause[i][0]
+#            root = self.tuples.data[predicate]
+#            found = False
+#            for k in range(1, len(clause[i])):
+#                j = clause[i][k]
+#                if type(j) == Atom:
+#                    if j not in root:
+#                        break
+#                    else:
+#                        root = root[j]
+#                    if len(root) == 0:
+#                        found = True                    
+#            if found == False:
+#                return False
+#        return True
+
+    def satisfy_clause(self, clause, variables={}):
+        def recursive(clause_pos, atom_pos, variables, root):
+            if atom_pos == 0: #it is a predicate
+                predicate = clause[clause_pos][0]
+                return recursive(clause_pos, atom_pos+1, dict(variables), self.tuples.data[predicate])
+            else:
+                j = clause[clause_pos][atom_pos]
+                if type(j) == Variable and j in variables:
+                    j = variables[j]
+                if type(j) == Atom:
+                    if j not in root:
+                        return False
+                    else:
+                        if atom_pos+1 == len(clause[clause_pos]):
+                            if clause_pos+1 == len(clause):
+                                return True
+                            else:
+                                return recursive(clause_pos+1, 0, dict(variables), root)
+                        else:
+                            return recursive(clause_pos, atom_pos+1, dict(variables), root[j])
+                elif type(j) == Variable: # new variable
+                    typ = self.modes[str(clause[clause_pos][0])][atom_pos-1]
+                    found = False
+                    for key, new_atom in self.atoms[typ].items():
+                        new_dict = dict(variables)
+                        new_dict[j] = new_atom
+                        found = recursive(clause_pos, atom_pos, new_dict, root)
+                        if found == True:
+                            break
+                    return found
+        return recursive(0, 0, variables, 0)
+
+    def count_satisfy_rule(self, head, body):
+        self.trues = 0
+        def recursive(variables, root):
+            if len(root) == 0:
+                start(variables)
+            else:
+                for tupl in root:
+                    new_variables = list(variables)
+                    new_variables.append(tupl)
+                    recursive(new_variables, root[tupl])
+        def start(variables):
+            #lista2 = [str(x) for x in variables]
+            #print(lista2)
+            dict_variables = {head[x]:variables[x-1] for x in range(1, len(head))}
+            st = self.satisfy_clause(body, dict_variables)
+            if st == True:
+                self.trues += 1
+        tuples = self.tuples.data[head[0]]
+        recursive([], tuples)
+        return self.trues
+
 g = Structure()
 n = 0
 subjects = set()
@@ -117,23 +221,15 @@ c = g.atoms['sportsteam']['buckeyes']
 
 #rule = [[g.predicates['athleteplaysforteam'], a,b], [g.predicates['athleteledsportsteam'], a,c], [g.predicates['teamalsoknownas'], b,c]]
 #rule = [[g.predicates['athleteplaysforteam'], a,b]]
-rule = [[g.predicates['athleteplaysforteam'], a,b], [g.predicates['athleteledsportsteam'], a,b]]
+head = [g.predicates['athleteplaysforteam'], a,b]
+body = [[g.predicates['athleteledsportsteam'], a,b]]
+rule = [head, [g.predicates['athleteledsportsteam'], a,b]]
+
+rule = [head, [g.predicates['athleteledsportsteam'], a, Variable('A')]]
 
 target = 'teamalsoknownas'
 
-def satisfy(rule, g):
-    for cl in rule:
-        root = g.tuples.data[cl[0]]
-        #print(cl[0].name)
-        for i in range(1, len(cl)):
-            #print(cl[i].name)
-            if cl[i] in root:
-                root = root[cl[i]]
-            else:
-                return False
-    return True
-        
-satisfy(rule, g)
+a = g.count_satisfy_rule([Predicate('athleteplaysforteam'), Variable('A'), Variable('B')], [[Predicate('athleteledsportsteam'), Variable('A'), Variable('C')], [Predicate('teamplaysagainstteam'), Variable('B'), Variable('C')]])
 
 #def return_head_var_assigns(target):
 #    list_assigns = []
